6e782681b53945d5dea19bca46c61649fbc21118
Franz Xanadu <30300977+FranzXanadu@users.noreply.github.com>
2024-08-15 09:53:10 -0500

    Cambio a vite

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  base: 'https://FranzXanadu.github.io/AprendeSinLimites_Intranet_r',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
--- vue.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    publicPath : "AprendeSinLimites_Intranet_r"
-}
